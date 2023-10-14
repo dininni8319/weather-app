4f3d527420198f0daa9142c6a1f0ca407c388a50
Salvatore Dininni <72815104+dininni8319@users.noreply.github.com>
2023-10-14 21:41:12 +0200

    Update SidebarWeatherDetail.tsx

--- src/components/SidebarWeatherDetail.tsx
+++ src/components/SidebarWeatherDetail.tsx
@@ -20,7 +20,7 @@ const SidebarWeatherDetail = ({
   const { isDarkMode } = useTheme()
   return (
     <div className={classes['weather-detail']}>
-      <div className={`${isDarkMode ? 'bg-[#ACC9DD] p-5 rounded-md text-black' : "bg-white p-5 rounded-md"}`}>
+      <div className={`${isDarkMode ? 'bg-[#ACC9DD] p-3 rounded-md text-black text-center' : "bg-white p-3 rounded-md text-center"}`}>
           <h2 className='capitalize text-2xl'>{place?.length < 20 ? place : place?.slice(0, 12)}{"..."}</h2>
           <h2 className='text-5xl'>{weather && weather[0]  +'°'}</h2>
           <p className='capitalize'>{currentWeather?.weatherPrimary}</p>
@@ -35,7 +35,9 @@ const SidebarWeatherDetail = ({
           <div>
             <p>{convertDate(currentWeather.dateTimeISO)} </p>
           </div>
-
+          <div className='flex justify-center'>
+            <img src={`../../public/weather-icons/${currentWeather.icon}`} alt="weather icon" />
+          </div>
       </div>
     </div>
   )
