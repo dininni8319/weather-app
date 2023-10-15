759baee444fe45b7e40dba98d241128ad5a4c0be
Salvatore Dininni <72815104+dininni8319@users.noreply.github.com>
2023-10-15 13:23:39 +0200

    added skeleton style

--- src/main.tsx
+++ src/main.tsx
@@ -3,17 +3,17 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { ThemeProvider } from './Context/ThemeContext.tsx'
-// import { SkeletonTheme } from "react-loading-skeleton"
+import { SkeletonTheme } from "react-loading-skeleton"
 import 'react-loading-skeleton/dist/skeleton.css' 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-      <ThemeProvider>
-        <App />
-      </ThemeProvider>
-      {/* <SkeletonTheme
+      <SkeletonTheme
         baseColor="#989898f0" highlightColor="#bfbabaf5"
-      > */}
-      {/* </SkeletonTheme> */}
+      >
+        <ThemeProvider>
+          <App />
+        </ThemeProvider>
+      </SkeletonTheme>
   </React.StrictMode>,
 )
